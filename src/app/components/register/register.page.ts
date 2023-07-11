c335cacbe502e0768157d1d49d33aaf5f92580d9
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael.local>
2023-07-11 17:44:49 -0300

    loginregister

--- src/app/components/register/register.page.ts
+++ src/app/components/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
@@ -10,35 +11,62 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  // Properties to bind to form fields
-  email = '';
-  password = '';
-  displayName = '';
+  registerForm: FormGroup;
 
-  constructor(private userService: UserService, private authService: AuthService, private router: Router) { }
-
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      displayName: ['', Validators.required]
+    });
   }
 
+  ngOnInit() {}
+
   async register() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+  
     try {
+      const { email, password, displayName } = this.registerForm.value;
+  
       // Register user with Firebase Auth
-      const firebaseUser = await this.authService.register(this.email, this.password);
-
-      // Create new User document in Firestore
-      const user: User = new User(
-        firebaseUser.uid,
-        this.email,
-        this.displayName
-      );
-
-      await this.userService.createUser(user);
-
-      // Navigate to the login page 
-      this.router.navigate(['/login']); 
-    } catch (error) {
-      console.log(error);
-      // Show error to user
+      const firebaseUser = await this.authService.register(email, password);
+  
+      if (firebaseUser) {
+        // Create new User document in Firestore
+        const user: User = new User(
+          firebaseUser.uid,
+          email,
+          displayName
+        );
+  
+        const userData = {
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName
+        };
+  
+        await this.userService.createUser(userData);
+  
+        // Navigate to the login page
+        this.router.navigate(['/login']);
+      }
+    } catch (error: any) {
+      if (error.code === 'auth/email-already-in-use') {
+        // Handle the case where the email is already in use
+        console.log('Email already in use. Please choose a different email or login with your existing account.');
+        // Show appropriate error message to the user
+      } else {
+        console.log(error);
+        // Show general error message to the user
+      }
     }
   }
 }
