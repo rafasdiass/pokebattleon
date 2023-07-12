4b9976026bc319d7e959f810210254f2853ffc09
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael.local>
2023-07-12 19:48:08 -0300

    login

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -5,22 +5,27 @@ import { take, map, tap } from 'rxjs/operators';  // Adicionado 'tap' aqui
 import { AuthService } from './auth.service';
 
 @Injectable({
-providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-canActivate(): Observable<boolean> {
- return this.authService.user$.pipe(
-   take(1),
-   map(user => !!user), // Convert 'user' to a boolean
-   tap((loggedIn: boolean) => {  // Tipo boolean adicionado
-     if (!loggedIn) {
-       console.log('access denied')
-       this.router.navigate(['/dashboard']);
-     }
-   }),
- );
-}
+  canActivate(): Observable<boolean> {
+    return this.authService.user$.pipe(
+      take(1),
+      map(user => !!user), // Convert 'user' to a boolean
+      tap((loggedIn: boolean) => {  // Tipo boolean adicionado
+        if (loggedIn) {  // Se o usuário estiver logado
+          console.log('access granted')
+          this.router.navigate(['/dashboard']);  // Navegar para a página de dashboard
+        } else {
+          console.log('access denied');
+          // Aqui, você pode redirecionar para a página de login, se desejar.
+          this.router.navigate(['/login']); 
+          // this.router.navigate(['/login']);
+        }
+      }),
+    );
+  }
 }
